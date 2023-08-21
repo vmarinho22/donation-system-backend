45d6d9774cc6dac1f599190e3c8897fd745fdb5d
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-08-21 17:04:40 -0300

    feat: adding orm to project

---
+++ src/clients/db.ts
@@ -0,0 +1,8 @@
+import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import env from '../config/env';
+
+const queryClient = postgres(env.DB_CONNECTION_STRING);
+const dbClient: PostgresJsDatabase = drizzle(queryClient);
+
+export default dbClient;
