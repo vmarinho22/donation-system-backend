c679192dc0a44fdf5c26069feba3b447c701e4c8
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-08-21 16:20:37 -0300

    feat: adding test lib

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+  collectCoverageFrom: ['src/**', '!src/index.ts', '!src/types/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  setupFiles: ['dotenv/config', '<rootDir>/tests/config/env.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+};
