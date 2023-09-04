7a8f4f74fbd2c27300ae92c0506015722c8fa4f5
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-09-04 15:36:39 -0300

    test: adding mock env test

--- tests/config/env.ts
+++ tests/config/env.ts
@@ -1,1 +1,4 @@
 process.env.NODE_ENV = 'test';
+process.env.PORT = '8001';
+process.env.SECRET = 'ce405462-d4e8-420c-ba3c-c00d356f3ff7';
+process.env.PG_STRING_CONNECTION = 'postgres://db:db@db/db';
