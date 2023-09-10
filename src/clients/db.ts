012ced30b74bbd1e293574226d432519a65f8942
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-09-09 23:30:37 -0300

    chore(db): adding ssl option

--- src/clients/db.ts
+++ src/clients/db.ts
@@ -2,7 +2,9 @@ import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import env from '../config/env';
 
-const queryClient = postgres(env.DB_CONNECTION_STRING);
+const queryClient = postgres(env.DB_CONNECTION_STRING, {
+  ssl: "prefer",
+});
 const dbClient: PostgresJsDatabase = drizzle(queryClient);
 
 export default dbClient;
