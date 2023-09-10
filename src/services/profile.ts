e3233c10218d1c7e74b797e12d5bfbef257d6c9c
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-09-10 00:14:50 -0300

    feat(profile): adding a profile service

---
+++ src/services/profile.ts
@@ -0,0 +1,78 @@
+import { z } from "zod";
+import dbClient from '../clients/db';
+import { profiles } from "../db/schema/profiles";
+import { eq } from "drizzle-orm";
+
+const profileSchema = z.object({
+  id: z.string().uuid(),
+  firstName: z.string(),
+  lastName: z.string(),
+  socialName: z.string().nullable(),
+  lang: z.enum(['pt_br', 'en_us']).nullable(),
+  photoUrl: z.string(),
+  phone: z.string(),
+  addressId: z.string().uuid(),
+  createdAt: z.date().nullable(),
+  updatedAt: z.date().nullable(),
+});
+
+export type Profile = z.infer<typeof profileSchema>;
+
+const createProfileDtoSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  socialName: z.string().nullable(),
+  lang: z.enum(['pt_br', 'en_us']).nullable(),
+  photoUrl: z.string(),
+  phone: z.string(),
+  addressId: z.string().uuid(),
+});
+
+type CreateProfileDto = z.infer<typeof createProfileDtoSchema>;
+
+async function create(profile: CreateProfileDto): Promise<Profile | null> {
+  const parsedProfile = createProfileDtoSchema.parse(profile);
+
+  const returnedProfile = await dbClient.insert(profiles).values(parsedProfile);
+
+  if (returnedProfile.length === 0) return null;
+
+  return returnedProfile[0];
+}
+
+async function getUnique(id: string): Promise<Profile | null> {
+  const idSchema = z.string().uuid();
+
+  const parsedId = idSchema.parse(id);
+
+  const returnedProfile = await dbClient.select().from(profiles).where(eq(profiles.id, parsedId));
+
+  if (returnedProfile.length === 0) return null;
+
+  return returnedProfile[0];
+}
+
+async function getAll(): Promise<Profile[]> {
+  const returnedProfiles = await dbClient.select().from(profiles);
+
+  return returnedProfiles;
+}
+
+async function update(id: string, profile: Partial<Profile>): Promise<boolean | null> {
+  const returnedProfile = await getUnique(id);
+
+  if (returnedProfile === null) return null;
+
+  const partialProfile = profileSchema.partial().parse(profile);
+
+  await dbClient.update(profiles).set(partialProfile).where(eq(profiles.id, returnedProfile.id));
+
+  return true;
+}
+
+export default {
+  create,
+  getUnique,
+  getAll,
+  update
+}
---
+++ tests/services/profile.spec.ts
@@ -0,0 +1,127 @@
+import { langEnum } from './../../src/types/lang.d';
+import profileService from '../../src/services/profile';
+import dbClient from '../../src/clients/db';
+
+jest.mock('../../src/clients/db');
+
+describe('profile service', () => {
+  let mockSelect: jest.Mock;
+  let mockFrom: jest.Mock;
+  let mockInsert: jest.Mock;
+  let mockValues: jest.Mock;
+  let mockUpdate: jest.Mock;
+  let mockSet: jest.Mock;
+
+  const id = "184e3edc-f1d3-4844-8766-310701a3eae7"
+
+  const mockedSentProfile = {
+    firstName: "Test",
+    lastName: "Test",
+    socialName: null,
+    lang: "pt_br" as langEnum.PT_BR,
+    photoUrl: "https://test.com",
+    phone: "999999999",
+    addressId: id,
+  };
+
+  const mockedReturnedProfile = {
+    id,
+    ...mockedSentProfile,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+
+  beforeEach(() => {
+    mockSelect = jest.fn().mockReturnThis();
+    mockFrom = jest.fn().mockReturnThis();
+
+    mockInsert = jest.fn().mockReturnThis();
+    mockValues = jest.fn().mockReturnThis();
+    
+    mockUpdate = jest.fn().mockReturnThis();
+    mockSet = jest.fn().mockReturnThis();
+
+    dbClient.select = mockSelect;
+    dbClient.insert = mockInsert;
+    dbClient.update = mockUpdate;
+  });
+
+  describe('create', () => {
+    it('should create and return a profile', async () => {
+      mockInsert.mockImplementation(() => ({
+        values: mockValues,
+      }));
+      mockValues.mockImplementation(() => [mockedReturnedProfile]);
+
+      const profile = await profileService.create(mockedSentProfile);
+
+      expect(profile).toBeDefined();
+      expect(profile).toEqual(mockedReturnedProfile);
+      expect(mockInsert).toBeCalledTimes(1);
+      expect(dbClient.insert).toBeCalled();
+    });
+  });
+
+  describe('getUnique', () => {
+    it('should return a profile', async () => {
+      mockSelect.mockImplementation(() => ({
+        from: mockFrom,
+      }));
+  
+      mockFrom.mockImplementation(() => ({
+        where: jest.fn().mockResolvedValue([mockedReturnedProfile]),
+      }));
+  
+      const profile = await profileService.getUnique(mockedReturnedProfile.id);
+      
+      expect(profile).toBeDefined();
+      expect(profile).toEqual(mockedReturnedProfile);
+      expect(mockSelect).toBeCalledTimes(1);
+      expect(dbClient.select).toBeCalled();
+    });
+  });
+
+  describe('getAll', () => {
+    it('should return all profiles', async () => {
+      mockSelect.mockImplementation(() => ({
+        from: jest.fn().mockResolvedValue([mockedReturnedProfile]),
+      }));
+  
+      const profiles = await profileService.getAll();
+
+      expect(profiles).toBeDefined();
+      expect(profiles).toEqual([mockedReturnedProfile]);
+      expect(mockSelect).toBeCalledTimes(1);
+      expect(dbClient.select).toBeCalled();
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return a profile', async () => {
+      mockSelect.mockImplementation(() => ({
+        from: mockFrom,
+      }));
+  
+      mockFrom.mockImplementation(() => ({
+        where: jest.fn().mockResolvedValue([mockedReturnedProfile]),
+      }));
+
+      mockUpdate.mockImplementation(() => ({
+        set: mockSet,
+      }));
+  
+      mockSet.mockImplementation(() => ({
+        where: jest.fn().mockResolvedValue([mockedReturnedProfile]),
+      }));
+  
+      const profile = await profileService.update(mockedReturnedProfile.id, mockedSentProfile);
+      
+      expect(profile).toBeDefined();
+      expect(profile).toBeTruthy();
+      expect(mockSelect).toBeCalledTimes(1);
+      expect(mockUpdate).toBeCalledTimes(1);
+      expect(dbClient.select).toBeCalled();
+      expect(dbClient.update).toBeCalled();
+    });
+  });
+});
