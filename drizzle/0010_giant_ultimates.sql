dc7a5f3cfd9fda591fe7b299c59e0f752be8cadd
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-10-06 00:59:26 -0300

    feat: adding disabled flag to doctor and nurse

---
+++ drizzle/0010_giant_ultimates.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "doctors" ADD COLUMN "disabled_at" timestamp;--> statement-breakpoint
+ALTER TABLE "nurses" ADD COLUMN "disabled_at" timestamp;
--- src/db/schema/nurses.ts
+++ src/db/schema/nurses.ts
@@ -1,4 +1,4 @@
-import { uuid, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { uuid, pgTable, timestamp, varchar, boolean } from 'drizzle-orm/pg-core';
 import { users } from './users';
 
 export const nurses = pgTable('nurses', {
@@ -7,6 +7,8 @@ export const nurses = pgTable('nurses', {
   specialty: varchar('specialty', { length: 255 }).notNull(),
   subspecialties: varchar('subspecialties', { length: 255 }),
   emergencyTelContact: varchar('emergency_tel_contact', { length: 255 }).notNull(),
+  disabled: boolean('disabled').default(false),
+  disabledAt: timestamp('disabled_at'),
   userId: uuid('user_id').references(() => users.id).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
